7cdb3462368cc0727924c766e6a15ad9988feb77
Manuel Selle <m.selle2@gmx.de>
2025-10-06 18:24:29 +0200

    First Commit

---
+++ src/main/java/htw/webtech/CalorieTrackzFE/CalorieTrackzFeApplication.java
@@ -0,0 +1,13 @@
+package htw.webtech.CalorieTrackzFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackzFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieTrackzFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/webtech/CalorieTrackzFE/CalorieTrackzFeApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.CalorieTrackzFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackzFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
